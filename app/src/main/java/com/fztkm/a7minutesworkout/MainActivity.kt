5788ed119e19b1687ebe2866ea66c05a293e4836
Takumi Fukuzawa <forthune.0517@gmail.com>
2022-01-12 11:09:46 +0900

    flStartにrippleエフェクト追加

--- app/src/main/java/com/fztkm/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/fztkm/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,22 @@ package com.fztkm.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.fztkm.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val flStart: FrameLayout = binding.flStart
+        flStart.setOnClickListener{
+            Toast.makeText(this, "Here we will start exercise",
+            Toast.LENGTH_LONG).show()
+        }
     }
 }
